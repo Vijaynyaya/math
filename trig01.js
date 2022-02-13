d4670cd9ea060885bbefaf0ea912df097489af05
Vijay Kumar <vijaynyaya@gmail.com>
2022-02-13 18:30:36 +0530

    add comments

--- trig01.js
+++ trig01.js
@@ -4,16 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
     var ctx = canvas.getContext("2d");
     var width = canvas.width = window.innerWidth;
     var height = canvas.height = window.innerHeight;
-    ctx.translate(10, height / 2); // shift the context downwards
-    ctx.scale(1, -1); // vertically flip the context
+    // center the sine wave
+    ctx.translate(10, height / 2); // translate the context downwards because y will be negative at some points
+    ctx.scale(1, -1); // vertically flip the context because in the browser top-left corner is the origin
     var computation = {};
-    // draw a sine wave
+    /**
+     * draw a sine wave when the hypotenuse is 100 units long.
+    */
     for (var radians = 0; radians < Math.PI * 2; radians += 0.25) {
         var sine = Math.sin(radians);
         var y = sine * 100;
         var x = radians * 100;
-        computation["radians(".concat(radians, ")")] = { sine: sine, x: x, y: y };
+        // draw a point
         ctx.fillRect(x, y, 3, 3);
+        computation["radians(".concat(radians, ")")] = { sine: sine, x: x, y: y };
     }
     // log the values computed values in browser's console.
     console.table(computation);
--- trig01.ts
+++ trig01.ts
@@ -5,17 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const width = canvas.width = window.innerWidth;
     const height = canvas.height = window.innerHeight;
     
-    ctx.translate(10, height / 2) // shift the context downwards
-    ctx.scale(1, -1); // vertically flip the context
+    // center the sine wave
+    ctx.translate(10, height / 2) // translate the context downwards because y will be negative at some points
+    ctx.scale(1, -1); // vertically flip the context because in the browser top-left corner is the origin
     
     const computation: any = {}
-    // draw a sine wave
+    /**
+     * draw a sine wave when the hypotenuse is 100 units long.
+    */
     for (let radians = 0; radians < Math.PI * 2; radians += 0.25) {
         const sine = Math.sin(radians)
         const y = sine * 100;
         const x = radians * 100;
-        computation[`radians(${radians})`] = { sine, x, y }
+        // draw a point
         ctx.fillRect(x,y,3,3)
+        
+        computation[`radians(${radians})`] = { sine, x, y }
     }
     // log the values computed values in browser's console.
     console.table(computation)
